67ab0749982d23721e0d1390777cd14e30c4d87b
PMengler <114950259+PMengler@users.noreply.github.com>
2023-03-22 21:39:07 -0700

    basic file structure with navbar and main pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Have a snazzy background image and some dynamic visual content here</p>
+    </div>
+  );
+}
